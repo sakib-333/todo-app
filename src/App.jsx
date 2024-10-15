0b59f7e59d9ed0e34899a47dfd0fc5533c68636e
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-10-15 16:22:24 +0600

    Add daisyUI.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1 className="text-5xl font-bold bg-red-400">Hello React</h1>
+      <button className="btn btn-outline">Click</button>
     </div>
   );
 }
