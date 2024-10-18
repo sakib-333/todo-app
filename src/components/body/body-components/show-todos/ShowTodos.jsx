63811c61f7354402f476e845dbdaadcd2a1a66cb
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-10-18 16:51:23 +0600

    Update mobile view.

--- src/components/body/body-components/show-todos/ShowTodos.jsx
+++ src/components/body/body-components/show-todos/ShowTodos.jsx
@@ -32,21 +32,23 @@ function ShowTodos() {
 
   return (
     <div className="mx-auto">
-      <div className="text-center grid grid-cols-6 border-b-2 pb-3">
-        <h1 className="font-bold">#</h1>
-        <h1 className="font-bold">Title</h1>
-        <h1 className="font-bold">Completed</h1>
-        <h1 className="font-bold">View</h1>
-        <h1 className="font-bold">Edit</h1>
-        <h1 className="font-bold">Delete</h1>
+      <div className="text-center grid grid-cols-5 md:grid-cols-6 gap-3 border-b-2 pb-3">
+        <h1 className="font-bold text-[12px] md:text-base hidden md:block">
+          #
+        </h1>
+        <h1 className="font-bold text-[12px] md:text-base">Title</h1>
+        <h1 className="font-bold text-[12px] md:text-base">Completed</h1>
+        <h1 className="font-bold text-[12px] md:text-base">View</h1>
+        <h1 className="font-bold text-[12px] md:text-base">Edit</h1>
+        <h1 className="font-bold text-[12px] md:text-base">Delete</h1>
       </div>
       {todos.map((todo) => {
         return (
           <div
             key={todo.id}
-            className="text-center grid grid-cols-6 gap-2 items-center py-3 border-b"
+            className="text-center grid grid-cols-5 md:grid-cols-6 gap-3 items-center py-3 border-b"
           >
-            <h1>{todoSequence++}</h1>
+            <h1 className="hidden md:block">{todoSequence++}</h1>
             <h1>{todo.title}</h1>
             <label
               className="flex items-center justify-center"
